0883c6ffdee32959cdf53c3a51d4d72133191551
William Arnaud Fotso <will.fotso.pro@gmail.com>
2025-07-14 10:33:19 -0500

    Merge branch 'development' into 'client' (#5)
    * feat(server) feat/ls Course workflow :
    	- Create available (NO other operation)
    	- Createcourse handler
    	- Fix on login handler (for local storage)
    
    * fix(shared) ; Fix Merge #4

--- internal/services/client/courses.go
+++ internal/services/client/courses.go
@@ -1,11 +1,10 @@
 package client
 
 import (
-
 	"bytes"
 	"encoding/json"
 	"errors"
-  
+
 	"fmt"
 	"io"
 	"net/http"
@@ -46,11 +45,9 @@ func GetCourses() ([]map[string]string, error) {
 		StatusName: assignmentData["status_name"],
 	}*/
 	var response struct {
-
 		Message string              `json:"message"`
 		Courses []map[string]string `json:"courses"`
 		Error   string              `json:"error,omitempty"`
-
 	}
 
 	isOnline := network.IsOnline()
@@ -81,7 +78,7 @@ func GetCourses() ([]map[string]string, error) {
 
 		if response.Error != "" {
 
-			return nil, errors.New(rI esponse.Error)
+			return nil, errors.New(response.Error)
 		}
 
 		if response.Courses == nil {
@@ -103,7 +100,6 @@ func GetCourses() ([]map[string]string, error) {
 
 	return response.Courses, nil
 }
-
 
 func CreateCourse(courseData map[string]string) (map[string]string, error) {
 
@@ -130,7 +126,6 @@ func CreateCourse(courseData map[string]string) (map[string]string, error) {
 		Name:       courseData["name"],
 		Duration:   courseData["duration"],
 		RoomNumber: courseData["room_number"],
-
 	}
 
 	isOnline := network.IsOnline()
@@ -141,7 +136,6 @@ func CreateCourse(courseData map[string]string) (map[string]string, error) {
 		if err != nil {
 			return nil, err
 		}
-
 
 		jsonData, _ := json.Marshal(courseData)
 
@@ -163,11 +157,9 @@ func CreateCourse(courseData map[string]string) (map[string]string, error) {
 		}
 
 		var response struct {
-
 			Message string            `json:"message"`
 			Course  map[string]string `json:"course"`
 			Error   string            `json:"error,omitempty"`
-
 		}
 
 		if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
@@ -201,6 +193,7 @@ func CreateCourse(courseData map[string]string) (map[string]string, error) {
 	return c.ToMap(), nil
 }
 
+/*
 func UpdateAssignment(id, column, value string) error {
 	new_client, err := NewClient()
 	if err != nil {
@@ -233,4 +226,5 @@ func UpdateAssignment(id, column, value string) error {
 	}
 
 	return nil
-}*/
+}
+*/
